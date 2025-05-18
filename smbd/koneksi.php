bb0c02411e5ee11fb616221d79e92df2d306b155
23-054 Bagas Wira K. <144298854+Baswiraa@users.noreply.github.com>
2025-05-19 02:29:28 +0700

    Delete smbd directory

--- smbd/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "db_shoes");
-
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-} else {
-    echo "Koneksi Berhasil";
-}
-?>
